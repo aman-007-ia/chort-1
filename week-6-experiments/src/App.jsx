f447965bfe6c1ce77d0f741e7321f61eee2f28c5
vineetha <yenugulavineetha2001@gmail.com>
2025-07-28 18:40:01 +0530

    useffect basic

--- week-6-experiments/src/App.jsx
+++ week-6-experiments/src/App.jsx
@@ -1,30 +1,35 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+  setInterval(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then(async (response) => {
+        const data = await response.json();
+        setTodos(data);
+      });
+  }, 10000);
+
+  // return () => clearInterval(intervalId); // cleanup
+}, []);
 
   return(
     <>
-      <CardWrapper>
-        <CardWrapper>
-          <TextComp />
-        </CardWrapper>
-      </CardWrapper>
-      <CardWrapper>
-        Hi there, I am a card component!
-      </CardWrapper>
+      {todos.map((todo) => (
+        <Todo key={todo.id} title ={todo.title} description = {todo.description}>
+        </Todo>
+      ))}
     </>
   )
 }
-const CardWrapper = ({ children }) => {
-  return <div style={{border:"2px solid black",padding:20}}>
-    {children}
+const Todo = ({ title,description}) => {
+  return <div>
+    <h1>{title}</h1>
+    <p>{description}</p>
   </div>
-  }
-  
-const TextComp = () => {
-  return <>
-    hi ther is a text component
-  </>
-  };
+}
+
 
 export default App
